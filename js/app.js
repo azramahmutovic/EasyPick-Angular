2c0c3af193eadf9e5fd7a6727242d0a8e6d1e5e1
azra <azramah6@gmail.com>
2016-05-11 14:45:57 +0200

    Objava oglasa

--- js/app.js
+++ js/app.js
@@ -198,9 +198,11 @@ app.controller('KorisnikController', function( myService, $scope, $window) {
     
   }]);
 
-  app.controller('OglasController', ['$http', '$window', '$log', function($http, $window, $log){
+  app.controller('OglasController', ['$http', '$window', '$log', '$location', function($http, $window, $log, $location){
     
     this.oglas = {};
+
+    this.drzave = ['Bosna i Hercegovina', 'Hrvatska', 'Crna Gora', 'Srbija'];
 
     this.objaviOglas = function(){
 
@@ -210,20 +212,31 @@ app.controller('KorisnikController', function( myService, $scope, $window) {
       'tip_oglasa': this.oglas.tip,
       'povrsina' : this.oglas.povrsina,
       'cijena': this.oglas.cijena,
+      'stanje' : this.oglas.stanje,
+      'drzava' : this.oglas.drzava,
       'grad' : this.oglas.grad,
-      'adresa' : this.oglas.adresa
-      
+      'adresa' : this.oglas.adresa,
+      'voda': this.oglas.voda,
+      'struja': this.oglas.struja,
+      'internet': this.oglas.internet,
+      'grijanje': this.oglas.grijanje,
+      'kablovska': this.oglas.kablovska,
+      'telefon': this.oglas.telefon,
+      'garaza': this.oglas.garaza
+
     }
 
     $http.post('http://localhost:8000/oglasi', data).success(function(data){
         
       alert("Oglas unesen");
-      $log.debug(angular.toJson(data, true));
+        $log.debug(angular.toJson(data, true));
+        //vrati se na pocetnu str
+        $location.path('/');
             
       })
       .error(function () {
             $log.debug(angular.toJson(data, true));
-
+            
         });
     };
   
