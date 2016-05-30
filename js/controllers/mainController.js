3a5dcdfe5e01623320364ee0e1f92c9f2833aab1
azra <azramah6@gmail.com>
2016-05-30 21:09:23 +0200

    Navbar ispravka

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -2,20 +2,15 @@ var app = angular.module('easypick');
 
 app.controller('mainController', [ '$window', '$scope', '$translate', '$http', '$log', function($window, $scope, $translate, $http, $log){
 
-    
-    $scope.isAdmin = false;
-
-    $http.get('http://localhost:8000/korisnik').success(function(data){
-        if(data.admin == 1)
-          $scope.isAdmin = true;
-        $log.debug(angular.toJson($scope.isAdmin, true));
-        
+    var id = $window.localStorage.getItem('user_id');
+    $http.get('http://localhost:8000/korisnici/' + id).success(function(data){
+    if(data.admin == 1)
+      $scope.isAdmin = true;
     })
     .error(function () {
         $log.debug(angular.toJson(data, true));
-
     });
-
+    
     this.userLoggedIn = function(){
     var token = $window.localStorage.getItem('token');
     return token ? true : false;
